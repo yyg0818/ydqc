6873b33ed14b29202267aaeb5ddff73e0a496756
yyg <yangyangang0818126@163.com>
2019-10-19 10:19:50 +0800

    first

---
+++ src/main/java/com/example/ydqc/YdqcApplication.java
@@ -0,0 +1,13 @@
+package com.example.ydqc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YdqcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YdqcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ydqc/YdqcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ydqc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YdqcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
